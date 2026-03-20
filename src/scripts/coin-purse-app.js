f96f12c2785658da2cd7d716abdd2585d8283a41
Federico Innocente <innocente.federico.97@gmail.com>
2026-03-20 13:03:23 +0100

    aggiornamento versione Application a V2

--- src/scripts/coin-purse-app.js
+++ src/scripts/coin-purse-app.js
@@ -1,42 +1,46 @@
 import * as Constants from "./utils/constants.js";
 
-export class CoinPurseApp extends Application {
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      id: "coin-purse-app",
+const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api;
+
+export class CoinPurseApp extends HandlebarsApplicationMixin(ApplicationV2) {
+  static DEFAULT_OPTIONS = {
+    id: "coin-purse-app",
+    window: {
       title: "Coin Purse",
-      template: "modules/coin-purse/src/templates/coin-purse.hbs",
-      width: 600,
-      height: "auto",
       resizable: true
-    });
-  }
+    },
+    position: {
+      width: 600,
+      height: "auto"
+    }
+  };
 
-  constructor(options = {}) {
-    super(options);
-  }
+  static PARTS = {
+    body: {
+      template: "modules/coin-purse/src/templates/coin-purse.hbs"
+    }
+  };
 
-  getData() {
+  async _prepareContext(_options) {
     const actor = game.user.character;
 
-    // Se non c'è un personaggio collegato
     if (!actor) {
       ui.notifications.warn(game.i18n.localize("labels.noCharacterSelected"));
-      return {};
+      return {
+        currency: {},
+        disableElectrum: game.settings.get("coin-purse", "disableElectrum")
+      };
     }
 
-    const currency = actor.system.currency ?? {};
+    const currency = foundry.utils.deepClone(actor.system.currency ?? {});
     const disableElectrum = game.settings.get("coin-purse", "disableElectrum");
 
-    // Filtra la valuta se electrum è disabilitato
-    const filteredCurrency = { ...currency };
-    if (disableElectrum) {
-      delete filteredCurrency.ep;
-    }
+    if (disableElectrum) delete currency.ep;
 
     return {
-      currency: filteredCurrency,
-      disableElectrum: disableElectrum
+      actor,
+      currency,
+      disableElectrum
     };
   }
 
@@ -47,60 +51,57 @@ export class CoinPurseApp extends Application {
       : Constants.CURRENCY_ORDER;
   }
 
-  _collectFormData(html) {
+  _collectFormData() {
     const data = {};
     const disableElectrum = game.settings.get("coin-purse", "disableElectrum");
-    const types = Constants.CURRENCY_ORDER;
-    if (disableElectrum) {
-      const index = types.indexOf("ep");
-      if (index >= 0) {
-        types.splice(index, 1);
-      }
+
+    const types = disableElectrum
+      ? Constants.CURRENCY_ORDER.filter(type => type !== "ep")
+      : [...Constants.CURRENCY_ORDER];
+
+    for (const type of types) {
+      const input = this.element.querySelector(`input[name="${type}"]`);
+      const val = Number.parseInt(input?.value ?? "0", 10);
+      data[type] = Number.isNaN(val) ? 0 : val;
     }
 
-    types.forEach(type => {
-      const val = parseInt(html.find(`input[name="${type}"]`).val());
-      data[type] = isNaN(val) ? 0 : val;
-    });
     return data;
   }
 
-  activateListeners(html) {
-    super.activateListeners(html);
+  _attachPartListeners(partId, htmlElement, options) {
+    super._attachPartListeners(partId, htmlElement, options);
 
-    // Logica applicativa
+    if (partId !== "body") return;
+
     const actor = game.user.character;
     if (!actor) return;
 
     // Handler del click sul bottone per aggiungere monete
-    html.find('button[name="receive"]').on("click", () => {
-      const updates = this._collectFormData(html);
+    htmlElement.querySelector('button[name="receive"]')?.addEventListener("click", () => {
+      const updates = this._collectFormData();
       this._applyCurrencyChange(actor, updates, true);
     });
 
     // Handler del click sul bottone per pagare
-    html.find('button[name="pay"]').on("click", () => {
-      const updates = this._collectFormData(html);
+    htmlElement.querySelector('button[name="pay"]')?.addEventListener("click", () => {
+      const updates = this._collectFormData();
       this._applyCurrencyChange(actor, updates, false);
     });
 
     // Handler del click sul bottone per convertire
-    html.find('button[name="convert"]').on("click", () => {
+    htmlElement.querySelector('button[name="convert"]')?.addEventListener("click", () => {
       this._minimizeCurrency(actor);
     });
   }
 
-  _applyCurrencyChange(actor, changes, isReceiving) {
-    const current = foundry.utils.duplicate(actor.system.currency);
+  async _applyCurrencyChange(actor, changes, isReceiving) {
+    const current = foundry.utils.deepClone(actor.system.currency ?? {});
 
-    // Gestione del caso di aggiunta monete
-    if (isReceiving) {
+    if (isReceiving) { // Gestione del caso di aggiunta monete
       for (const type in changes) {
         current[type] += changes[type];
       }
-
-      // Gestione del caso di pagamento con borrowing
-    } else {
+    } else { // Gestione del caso di pagamento con borrowing
       const currencyOrder = this._getCurrencyOrder();
       for (let i = 0; i < currencyOrder.length; i++) {
         const type = currencyOrder[i];
@@ -139,17 +140,20 @@ export class CoinPurseApp extends Application {
       }
     }
 
-    actor.update({ "system.currency": current }).then(() => {
-      this.element.find('input[type="number"]').val('');
-      this.render(false);
+    await actor.update({ "system.currency": current });
+
+    this.element.querySelectorAll('input[type="number"]').forEach(i => {
+      i.value = "";
     });
+
+    this.render();
   }
 
   /*** 
    * Funzione per minimizzare la valuta a quella più alta possibile, convertendo a cascata
   */
-  _minimizeCurrency(actor) {
-    const current = foundry.utils.duplicate(actor.system.currency);
+  async _minimizeCurrency(actor) {
+    const current = foundry.utils.deepClone(actor.system.currency ?? {});
     const currencyOrder = this._getCurrencyOrder();
 
     for (let i = 0; i < currencyOrder.length - 1; i++) {
@@ -163,9 +167,12 @@ export class CoinPurseApp extends Application {
       }
     }
 
-    actor.update({ "system.currency": current }).then(() => {
-      this.element.find('input[type="number"]').val('');
-      this.render(false);
+    await actor.update({ "system.currency": current });
+
+    this.element.querySelectorAll('input[type="number"]').forEach(i => {
+      i.value = "";
     });
+
+    this.render();
   }
 }
